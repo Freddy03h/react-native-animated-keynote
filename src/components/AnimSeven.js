b3ce133064e893d70ada42178eb8a4942e8cc127
freddy <freddy03h@gmail.com>
2019-06-04 20:40:51 +0200

    fix some slides

--- src/components/AnimSeven.js
+++ src/components/AnimSeven.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import { View, StyleSheet, TouchableOpacity, Animated } from 'react-native-web';
+import React from "react";
+import { View, StyleSheet, TouchableOpacity, Animated } from "react-native-web";
 
-export default class AnimFive extends React.PureComponent {
-  
+export default class AnimSeven extends React.PureComponent {
   myAnimatedValue = new Animated.Value(0);
-  
+
   playAnim = () => {
     Animated.sequence([
-      Animated.timing(
-        this.myAnimatedValue,
-        {
-          toValue: 100,
-          duration: 1000,
-        },
-      ),
-      Animated.timing(
-        this.myAnimatedValue,
-        {
-          toValue: 0,
-          duration: 1000,
-        },
-      ),
+      Animated.timing(this.myAnimatedValue, {
+        toValue: 100,
+        duration: 1000
+      }),
+      Animated.timing(this.myAnimatedValue, {
+        toValue: 0,
+        duration: 1000
+      })
     ]).start(() => {
       this.playAnim();
     });
@@ -29,88 +22,101 @@ export default class AnimFive extends React.PureComponent {
   componentDidMount() {
     this.playAnim();
   }
-    
+
   render() {
-    const translateInterpolate =
-      this.myAnimatedValue.interpolate({
-        inputRange: [0, 100],
-        outputRange: [-(0.5*300)/2, 0],
-      });
+    const translateInterpolate = this.myAnimatedValue.interpolate({
+      inputRange: [0, 100],
+      outputRange: [0, ((2 - 1) * 150) / 2]
+    });
 
-    const scaleInterpolate =
-      this.myAnimatedValue.interpolate({
-        inputRange: [0, 100],
-        outputRange: [0.5, 1],
-      });
+    const scaleInterpolate = this.myAnimatedValue.interpolate({
+      inputRange: [0, 100],
+      outputRange: [1, 2]
+    });
 
-    const scalePointInterpolate =
-      this.myAnimatedValue.interpolate({
-        inputRange: [0, 100],
-        outputRange: [2, 1],
-      });
+    const scalePointInterpolate = this.myAnimatedValue.interpolate({
+      inputRange: [0, 100],
+      outputRange: [1, 0.5]
+    });
 
     return (
       <View style={styles.container}>
         <Animated.View
-          style={[styles.box, styles.box2, {
-            transform: [{scale: scaleInterpolate}]
-          }]}
+          style={[
+            styles.box,
+            styles.box2,
+            {
+              transform: [{ scale: scaleInterpolate }]
+            }
+          ]}
         >
           <Animated.View
-            style={[styles.point, {
-              transform: [{scale: scalePointInterpolate}]
-            }]}
+            style={[
+              styles.point,
+              {
+                transform: [{ scale: scalePointInterpolate }]
+              }
+            ]}
           />
         </Animated.View>
 
         <Animated.View
-          style={[styles.box, {
-            transform: [
-              { translateY: translateInterpolate },
-              {scale: scaleInterpolate}
-            ]
-          }]}
+          style={[
+            styles.box,
+            {
+              transform: [
+                { translateY: translateInterpolate },
+                { scale: scaleInterpolate }
+              ]
+            }
+          ]}
         >
-        <Animated.View
-            style={[styles.point, styles.point2, {
-              transform: [{scale: scalePointInterpolate}]
-            }]}
+          <Animated.View
+            style={[
+              styles.point,
+              styles.point2,
+              {
+                transform: [{ scale: scalePointInterpolate }]
+              }
+            ]}
           />
         </Animated.View>
       </View>
     );
   }
 }
-    
+
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingTop: 75,
+    paddingBottom: 75
   },
   box: {
-    backgroundColor: '#ff4081',
-    width: 300,
-    height: 300
+    backgroundColor: "#ff4081",
+    width: 150,
+    height: 150
   },
   box2: {
-    backgroundColor: 'black',
+    backgroundColor: "black"
   },
   point: {
-    backgroundColor: '#ff4081',
+    backgroundColor: "#ff4081",
     width: 30,
     height: 30,
     borderRadius: 15,
-    position: 'absolute',
+    position: "absolute",
     top: "50%",
     left: "50%",
     marginLeft: -15,
-    marginTop: -15,
+    marginTop: -15
   },
   point2: {
-    backgroundColor: 'black',
-    position: 'absolute',
+    backgroundColor: "black",
+    position: "absolute",
     top: "0%",
     marginLeft: -15,
-    marginTop: -15,
-  },
+    marginTop: -15
+  }
 });
